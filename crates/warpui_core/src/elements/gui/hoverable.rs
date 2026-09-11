e73cbf8104f1052db9a96a68daaf328d849e2f3a
42piratas <42piratas@Darkseid.local>
2026-09-10 22:33:05 -0300

    fix: resolve strict clippy violations

--- crates/warpui_core/src/elements/gui/hoverable.rs
+++ crates/warpui_core/src/elements/gui/hoverable.rs
@@ -718,24 +718,19 @@ impl Element for Hoverable {
 
                 // The double-clicked handler takes precendence. However, we should still fall back to the single-click handler
                 // on a double-click if there's no double-click handler set.
-                if matches!(click_count, Some(2)) && self.double_click_handler.is_some() {
-                    let handler = self
-                        .double_click_handler
-                        .as_mut()
-                        .expect("handler should exist");
+                if let (Some(2), Some(handler)) = (click_count, self.double_click_handler.as_mut())
+                {
                     handler(ctx, app, *position);
                     ctx.notify();
                     return true;
-                } else if click_count.is_some() && self.click_handler.is_some() {
-                    let handler = self.click_handler.as_mut().expect("handler should exist");
+                } else if let (Some(_), Some(handler)) = (click_count, self.click_handler.as_mut())
+                {
                     handler(ctx, app, *position);
                     ctx.notify();
                     return true;
-                } else if click_count.is_some() && self.click_with_modifiers_handler.is_some() {
-                    let handler = self
-                        .click_with_modifiers_handler
-                        .as_mut()
-                        .expect("handler should exist");
+                } else if let (Some(_), Some(handler)) =
+                    (click_count, self.click_with_modifiers_handler.as_mut())
+                {
                     handler(ctx, app, *position, *modifiers);
                     ctx.notify();
                     return true;
@@ -750,10 +745,8 @@ impl Element for Hoverable {
                     return true;
                 }
             }
-            Event::LeftMouseDragged { .. } => {
-                if self.suppress_drag && self.state().is_clicked() {
-                    return true;
-                }
+            Event::LeftMouseDragged { .. } if self.suppress_drag && self.state().is_clicked() => {
+                return true;
             }
             _ => {}
         }
